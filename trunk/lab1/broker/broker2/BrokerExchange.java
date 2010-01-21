f24cb747be1e73bc9e41a2085200f4045f39973d
airforce1j <airforce1j@cdf39bd8-06d3-11df-8209-c32e8d2b4809>
2010-01-21 22:19:01 +0000

    good 4 now

--- trunk/lab1/broker/broker2/BrokerExchange.java
+++ trunk/lab1/broker/broker2/BrokerExchange.java
@@ -2,97 +2,121 @@ package broker.broker2;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.lang.*;
 
 public class BrokerExchange {
 	public static void main(String[] args) throws IOException,
 			ClassNotFoundException {
 
-		Socket brokerSocket = null;
+		Socket clientSocket = null;
 		ObjectOutputStream out = null;
 		ObjectInputStream in = null;
 
 		try {
 			/* variables for hostname/port */
-			// Hardcoded not allowed, but these will _never_ be used, and only
-			// exist for debugging
-			String hostname = "localhost";
-			int port = 4444;
-
-			if (args.length == 2) {
-				hostname = args[0];
-				port = Integer.parseInt(args[1]);
+			BrokerLocation serverinfo = new BrokerLocation("localhost", 4444);
+			//serverinfo.broker_host = "localhost";
+			//serverinfo.broker_port = 4444;
+			
+			if(args.length == 3 ) {
+				serverinfo.broker_host = args[0];
+				serverinfo.broker_port = Integer.parseInt(args[1]);
 			} else {
 				System.err.println("ERROR: Invalid arguments!");
 				System.exit(-1);
 			}
-			brokerSocket = new Socket(hostname, port);
+			clientSocket = new Socket(serverinfo.broker_host, serverinfo.broker_port);
 
-			out = new ObjectOutputStream(brokerSocket.getOutputStream());
-			in = new ObjectInputStream(brokerSocket.getInputStream());
+			out = new ObjectOutputStream(clientSocket.getOutputStream());
+			in = new ObjectInputStream(clientSocket.getInputStream());
 
 		} catch (UnknownHostException e) {
-			System.err.println("ERROR: Don't know where to connect!!");
+			System.err.println("ERROR: Don't know where to connedelimct!!");
 			System.exit(1);
 		} catch (IOException e) {
 			System.err.println("ERROR: Couldn't get I/O for the connection.");
 			System.exit(1);
 		}
 
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(
-				System.in));
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 		String userInput;
-
-		System.out.println("Enter queries or x for exit:");
-		System.out.print("> ");
-		
-		try {
-			while ((userInput = stdIn.readLine()) != null
-					&& userInput.toLowerCase().indexOf("bye") == -1) {
-
-				// If we receive "x", quit
-				if (userInput.equalsIgnoreCase("x")) {
-					break;
-				}
-
-				/* make a new request packet */
-				BrokerPacket packetToServer = new BrokerPacket();
-				packetToServer.type = BrokerPacket.BROKER_REQUEST;
-				packetToServer.symbol = userInput;
+		boolean validnum = false;
+//		end of int validation
+		System.out.print(">");
+		while ((userInput = stdIn.readLine()) != null && userInput.toLowerCase().indexOf("x") == -1) {
+			/* make a new request packet */
+			BrokerPacket packetToServer = new BrokerPacket();
+			packetToServer.symbol = userInput;
+			
+			//validate the integer value if present
+			StringTokenizer str = new StringTokenizer(userInput);
+			validnum = false;
+			
+			int i = -1;
+			
+			while(str.hasMoreElements()) {
+			    //extract the number here, IF found
+				i = Integer.parseInt(str.nextToken());
+			}
+			
+			if ((i <= 300) && (i >= 0)) {
+				validnum = true;
+			}
+			else {
+				validnum = false;
+			}
+			//end of int validation
+			
+			//Check what does the user want to do: ADD, UPDATE, REMOVE
+			if ((userInput.toLowerCase().indexOf("add") != -1) && validnum == true) {
+				packetToServer.type = BrokerPacket.EXCHANGE_ADD;
 				out.writeObject(packetToServer);
-
+			}
+			else if ((userInput.toLowerCase().indexOf("update") != -1) && validnum == true) {
+				packetToServer.type = BrokerPacket.EXCHANGE_UPDATE;
+				out.writeObject(packetToServer);
+			}
+			else if (userInput.toLowerCase().indexOf("remove") != -1) {
+				packetToServer.type = BrokerPacket.EXCHANGE_REMOVE;
+				out.writeObject(packetToServer);
+			}
+			else { //Error for invalid input
+				//packetToServer.type = BrokerPacket.ERROR_INVALID_EXCHANGE; // mark as such so that we dont expect a packet later, because we wont send any
+				System.out.println("Invalid command.\nUsage: add | update | remove <stock name>\n");
+			}
+		
+			//sending to server
+			if (packetToServer.type == BrokerPacket.EXCHANGE_REMOVE || packetToServer.type == BrokerPacket.EXCHANGE_UPDATE || packetToServer.type == BrokerPacket.EXCHANGE_ADD) {
 				/* print server reply */
 				BrokerPacket packetFromServer;
 				packetFromServer = (BrokerPacket) in.readObject();
-
-				String printQuote = "0";
-				if (packetFromServer.type == BrokerPacket.BROKER_QUOTE) {
-					// Sanitize received quote
-					if (packetFromServer.quote != null) {
-						printQuote = String.valueOf(packetFromServer.quote);
-					}
-						
-					System.out.println("Quote from broker: "
-							+ printQuote);
+	
+				if (packetFromServer.type == BrokerPacket.EXCHANGE_REPLY) { // will print only if non-NULL has been sent
+					System.out.println(packetFromServer.quote);
 				}
-					
-
+				else if (packetFromServer.type == BrokerPacket.ERROR_INVALID_SYMBOL){
+					System.out.println(packetFromServer.symbol + " invalid.\n");
+				}
+				else if (packetFromServer.type == BrokerPacket.ERROR_OUT_OF_RANGE){
+					System.out.println(packetFromServer.symbol + " out of range.\n");
+				}
+				else if(packetFromServer.type == BrokerPacket.ERROR_SYMBOL_EXISTS){
+					System.out.println(packetFromServer.symbol + " exists.\n");
+				}
 				/* re-print console prompt */
-				System.out.print("> ");
+				System.out.print(">");
 			}
-
-			/* tell server that i'm quitting */
-			BrokerPacket packetToServer = new BrokerPacket();
-			packetToServer.type = BrokerPacket.BROKER_BYE;
-			packetToServer.symbol = "Bye!";
-			out.writeObject(packetToServer);
-
-		} catch (SocketException e) {
-			System.out.println("Server has disconnected");
 		}
+		
+		/* tell server that i'm quitting */
+		BrokerPacket packetToServer = new BrokerPacket();
+		packetToServer.type = BrokerPacket.BROKER_BYE;
+		out.writeObject(packetToServer);
 
 		out.close();
 		in.close();
 		stdIn.close();
-		brokerSocket.close();
+		clientSocket.close();
 	}
 }
