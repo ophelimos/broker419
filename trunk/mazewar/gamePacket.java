0d274f274e2a93848112b982dbde91cfbf2d93c1
airforce1j <airforce1j@cdf39bd8-06d3-11df-8209-c32e8d2b4809>
2010-02-16 03:26:38 +0000

    gamePacket made .. not complete

---
+++ trunk/mazewar/gamePacket.java
@@ -0,0 +1,12 @@
+//We will use this packet to transmit across players
+/* TODO It consist of:- vector timestamps
+ * 				- type of movement the player has made 
+ */
+
+public class gamePacket {
+	
+	public int action;
+	
+	public timestamp tosend;
+	
+}
