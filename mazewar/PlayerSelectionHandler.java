7fafde068a03d5de2526e0a46b3439dff257937f
robinsonjpr@gmail.com <robinsonjpr@gmail.com@cdf39bd8-06d3-11df-8209-c32e8d2b4809>
2010-03-11 23:55:34 +0000

    Making robust

--- mazewar/PlayerSelectionHandler.java
+++ mazewar/PlayerSelectionHandler.java
@@ -5,34 +5,36 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 public class PlayerSelectionHandler implements ListSelectionListener {
-	
+
 	/**
 	 * Connection DB (where we pull our data out of)
 	 */
 	private ConnectionDB connectionDB;
-	
+
 	/**
 	 * The mazewar GUI where selectedPlayers is kept
 	 */
 	private MazewarGUI mazewarGUI;
-	
+
 	/**
 	 * Constructor
+	 * 
 	 * @param connectionDB
 	 * @param mazewarGUI
 	 */
-	public PlayerSelectionHandler(ConnectionDB connectionDB, MazewarGUI mazewarGUI) {
+	public PlayerSelectionHandler(ConnectionDB connectionDB,
+			MazewarGUI mazewarGUI) {
 		this.connectionDB = connectionDB;
 		this.mazewarGUI = mazewarGUI;
 	}
 
 	public void valueChanged(ListSelectionEvent e) {
-//		 Don't bother doing anything if they're still adjusting the list
+		// Don't bother doing anything if they're still adjusting the list
 		boolean isAdjusting = e.getValueIsAdjusting();
 		if (isAdjusting) {
 			return;
 		}
-		
+
 		// Every time our selection changes, remake the selectedPlayers vector
 		mazewarGUI.selectedPlayers.clear();
 		JList lsm = (JList) e.getSource();
@@ -40,9 +42,11 @@ public class PlayerSelectionHandler implements ListSelectionListener {
 		int maxIndex = lsm.getMaxSelectionIndex();
 		// This is dangerous, because it's handling the actual object
 		Vector<OutputPeer> availablePlayers = connectionDB.outputPeers;
-		for (int i = minIndex; i < maxIndex; i++) {
-			if (lsm.isSelectedIndex(i)) {
-				mazewarGUI.selectedPlayers.add(availablePlayers.get(i));
+		if (!availablePlayers.isEmpty()) {
+			for (int i = minIndex; i <= maxIndex; i++) {
+				if (lsm.isSelectedIndex(i)) {
+					mazewarGUI.selectedPlayers.add(availablePlayers.get(i));
+				}
 			}
 		}
 	}
