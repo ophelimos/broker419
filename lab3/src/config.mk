f71aad52a33f27bcc210334820ab0a8b6507482b
airforce1j <airforce1j@cdf39bd8-06d3-11df-8209-c32e8d2b4809>
2010-04-07 00:59:44 +0000

    doesnt work et

--- lab3/src/config.mk
+++ lab3/src/config.mk
@@ -12,9 +12,9 @@ CFLAGS=-g -O0
 CLIBS=-lpthread -lhermes -labacus /cad2/ece419s/db/lib/libdb-4.7.a
 #CLIBS=-lpthread -lhermes -labacus -ldb
 
-CODE_BASE=${HOME}/ece419/lab3
+CODE_BASE=${HOME}/workspace/lab3
 
-LIB_BASE=${HOME}/ece419/lab3
+LIB_BASE=${HOME}/workspace/lab3
 
 ABACUS_DIR=${LIB_BASE}/lib/abacus_c
 HERMES_DIR=${LIB_BASE}/lib/hermes_c
