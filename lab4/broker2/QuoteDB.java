ff5ffafb918b18321b26b834d6e5f98a445764f8
robinsonjpr@gmail.com <robinsonjpr@gmail.com@cdf39bd8-06d3-11df-8209-c32e8d2b4809>
2010-04-08 22:35:01 +0000

    Ported broker2

---
+++ lab4/broker2/QuoteDB.java
@@ -0,0 +1,195 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * A synchronized reader-biased class to manage the flat file stock quote
+ * database
+ * 
+ * @author James Robinson
+ * 
+ */
+public class QuoteDB {
+
+	// Access lock
+	ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+
+	// Actual database used to store key-value pairs
+	private Hashtable<String, Long> DB;
+
+	private String persistentFileName;
+
+	// Platform independent newline
+	public static String newline = System.getProperty("line.separator");
+
+	QuoteDB(String fileName) {
+		try {
+			this.open(fileName);
+		} catch (IOException e) {
+			System.out.println("Failed to open database file " + fileName);
+		}
+	}
+
+	QuoteDB() {
+		// Don't open anything
+		persistentFileName = null;
+		DB = null;
+	}
+
+	/**
+	 * Open the flat file containing the key-value pairs createTempFi
+	 * 
+	 * @param fileName
+	 */
+	public void open(String fileName) throws IOException {
+		// Lock it
+		lock.writeLock().lock();
+		try {
+			// Make sure it's not already open - if so, close it
+			if (DB != null) {
+				close();
+			}
+
+			// Now, make a new one
+			DB = new Hashtable<String, Long>();
+
+			// Set the persistent file name
+			persistentFileName = fileName;
+
+			// Read the quotes file into an internal hash table
+
+			Scanner quoteInput = new Scanner(new BufferedReader(new FileReader(
+					fileName)));
+
+			while (quoteInput.hasNext()) {
+
+				String symbol = quoteInput.next();
+				symbol = symbol.toLowerCase();
+				Long value = Long.parseLong(quoteInput.next());
+
+				DB.put(symbol, value);
+			}
+
+			quoteInput.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Can't find database file: " + e.getMessage()
+					+ " in directory " + System.getProperty("user.dir"));
+			System.exit(-1);
+		} catch (NoSuchElementException e) {
+			System.out.println("Error reading database file " + fileName);
+			System.out.println("Continuing");
+			/* quoteInput.close(); */
+		} finally {
+			lock.writeLock().unlock();
+		}
+	}
+
+	public void close() throws IOException {
+		// Acquire the lock
+		lock.writeLock().lock();
+
+		try {
+			// Flush database contents to disk
+			this.flush();
+			// Null out the pointer
+			DB = null;
+			// Clear the file name
+			persistentFileName = null;
+
+		} catch (IOException e) {
+			System.out.println("Failed to flush database!");
+			System.out.println("ERROR: " + e.getMessage());
+			throw e;
+		} finally {
+			lock.writeLock().unlock();
+		}
+	}
+
+	public void flush() throws IOException {
+
+		// Acquire the lock: I don't have to worry about trying to regrab a lock
+		// I hold, because the library will handle it for me.
+		lock.writeLock().lock();
+
+		// Write the DB to a temporary file first, and then overwrite, rather
+		// than accidentally truncating it
+		File cwd = new File(System.getProperty("user.dir"));
+		File tempFile = File.createTempFile("dbtemp", ".tmp", cwd);
+		BufferedWriter quoteOutput = new BufferedWriter(
+				new FileWriter(tempFile));
+
+		// Pull the key-value pairs out of the database
+		Enumeration<String> keys = DB.keys();
+
+		while (keys.hasMoreElements()) {
+			String curKey = keys.nextElement();
+			Long curValue = DB.get(curKey);
+			// Print it to the file
+			quoteOutput.write(curKey + " " + curValue + newline);
+		}
+
+		// Flush the temporary file
+		quoteOutput.flush();
+
+		// Now, move the old DB to a backup file
+		File dbFile = new File(persistentFileName);
+		String backupFileName = persistentFileName.concat(".bak");
+		File backupFile = new File(backupFileName);
+		boolean success = dbFile.renameTo(backupFile);
+		if (!success) {
+			IOException noRename = new IOException("Failed to rename file"
+					+ persistentFileName + " to " + backupFileName);
+			throw noRename;
+		}
+
+		// Rename the temporary file to the database file
+		success = tempFile.renameTo(dbFile);
+		if (!success) {
+			IOException noRename = new IOException(
+					"Failed to rename temp file to " + persistentFileName);
+			throw noRename;
+		}
+
+		// Unlock it
+		lock.writeLock().unlock();
+
+	}
+
+	public Long get(String key) {
+		lock.readLock().lock();
+		Long tmp = DB.get(key);
+		lock.readLock().unlock();
+		return tmp;
+	}
+
+	public void put(String key, Long value) {
+		lock.readLock().lock();
+		DB.put(key, value);
+		lock.readLock().unlock();
+	}
+
+	public boolean containsKey(String key) {
+		lock.readLock().lock();
+		boolean tmp = DB.containsKey(key);
+		lock.readLock().unlock();
+		return tmp;
+	}
+
+	public Long remove(String key) {
+		lock.readLock().lock();
+		Long tmp = DB.remove(key);
+		lock.readLock().unlock();
+		return tmp;
+	}
+
+}
--- lab4/broker2/Server.java
+++ lab4/broker2/Server.java
@@ -1,1 +1,120 @@
- 
+ import broker.Broker;
+
+public class Server {
+	// Table of quotes file
+	public static final String quoteDBFileName = "nasdaq";
+	
+	public static QuoteDB quoteDB = null;
+
+	static int run(org.omg.CORBA.ORB orb, String[] args)
+			throws org.omg.CORBA.UserException {
+		//
+		// Resolve Root POA
+		//
+		org.omg.PortableServer.POA rootPOA = org.omg.PortableServer.POAHelper
+				.narrow(orb.resolve_initial_references("RootPOA"));
+
+		//
+		// Get a reference to the POA manager
+		//
+		org.omg.PortableServer.POAManager manager = rootPOA.the_POAManager();
+
+		//
+		// Create implementation object
+		//
+		Broker_impl brokerImpl = new Broker_impl(rootPOA);
+		Broker broker = brokerImpl._this(orb);
+
+		//
+		// Save reference
+		//
+		try {
+			String ref = orb.object_to_string(broker);
+			String refFile = "Broker.ref";
+			java.io.FileOutputStream file = new java.io.FileOutputStream(
+					refFile);
+			java.io.PrintWriter out = new java.io.PrintWriter(file);
+			out.println(ref);
+			out.flush();
+			file.close();
+		} catch (java.io.IOException ex) {
+			System.err.println("broker.Server: can't write to `"
+					+ ex.getMessage() + "'");
+			return 1;
+		}
+
+		//
+		// Save reference as html
+		//
+		try {
+			String ref = orb.object_to_string(broker);
+			String refFile = "Broker.html";
+			java.io.FileOutputStream file = new java.io.FileOutputStream(
+					refFile);
+			java.io.PrintWriter out = new java.io.PrintWriter(file);
+			out.println("<applet codebase=\"classes\" "
+					+ "code=\"broker/Client.class\" " + "archive=\"OB.jar\" "
+					+ "width=500 height=300>");
+			out.println("<param name=ior value=\"" + ref + "\">");
+			out.println("<param name=org.omg.CORBA.ORBClass "
+					+ "value=com.ooc.CORBA.ORB>");
+			//
+			// No need to specify ORBSingletonClass - it's ignored anyway
+			//
+			// out.println("<param name=org.omg.CORBA.ORBSingletonClass " +
+			// "value=com.ooc.CORBA.ORBSingleton>");
+			out.println("</applet>");
+			out.flush();
+			file.close();
+		} catch (java.io.IOException ex) {
+			System.err.println("broker.Server: can't write to `"
+					+ ex.getMessage() + "'");
+			return 1;
+		}
+
+		//
+		// Run implementation
+		//
+		
+//		 Open the local database
+		QuoteDB quoteDB = new QuoteDB(quoteDBFileName);
+		
+		// Make things gets closed properly on exit
+		Runtime.getRuntime().addShutdownHook(new ShutdownThread(quoteDB));
+		
+		manager.activate();
+		orb.run();
+
+		return 0;
+	}
+
+	public static void main(String args[]) {
+		java.util.Properties props = System.getProperties();
+		props.put("org.omg.CORBA.ORBClass", "com.ooc.CORBA.ORB");
+		props.put("org.omg.CORBA.ORBSingletonClass",
+				"com.ooc.CORBA.ORBSingleton");
+		props.put("ooc.orb.id", "Broker-Server");
+
+		int status = 0;
+		org.omg.CORBA.ORB orb = null;
+
+		try {
+			orb = org.omg.CORBA.ORB.init(args, props);
+			status = run(orb, args);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			status = 1;
+		}
+
+		if (orb != null) {
+			try {
+				orb.destroy();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				status = 1;
+			}
+		}
+
+		System.exit(status);
+	}
+}
