9575426b0ebb99a62268e2df4ee54e2b1d22d50d
robinsonjpr@gmail.com <robinsonjpr@gmail.com@cdf39bd8-06d3-11df-8209-c32e8d2b4809>
2010-04-09 00:26:31 +0000

    remove and flush and close

--- lab4/broker3/QuoteDB.java
+++ lab4/broker3/QuoteDB.java
@@ -8,15 +8,11 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Enumeration;
-import java.util.Hashtable;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
-import jdbc.JDBCExample;
 
 /**
  * A synchronized reader-biased class to manage the flat file stock quote
@@ -82,7 +78,7 @@ public class QuoteDB {
 			Statement st = db.createStatement();
 			st.executeUpdate(createString);
 			st.close();
-			
+
 			/* Insert values from flat file */
 			try {
 
@@ -90,8 +86,8 @@ public class QuoteDB {
 				persistentFileName = fileName;
 
 				// Read the quotes file into an internal hash table
-				Scanner quoteInput = new Scanner(new BufferedReader(new FileReader(
-						fileName)));
+				Scanner quoteInput = new Scanner(new BufferedReader(
+						new FileReader(fileName)));
 
 				while (quoteInput.hasNext()) {
 
@@ -105,8 +101,9 @@ public class QuoteDB {
 				quoteInput.close();
 
 			} catch (FileNotFoundException e) {
-				System.out.println("Can't find database file: " + e.getMessage()
-						+ " in directory " + System.getProperty("user.dir"));
+				System.out.println("Can't find database file: "
+						+ e.getMessage() + " in directory "
+						+ System.getProperty("user.dir"));
 				System.exit(-1);
 			} catch (NoSuchElementException e) {
 				System.out.println("Error reading database file " + fileName);
@@ -125,25 +122,28 @@ public class QuoteDB {
 	}
 
 	public void close() throws IOException {
+		// Flush table to disk
+		this.flush();
+//		 Clear the file name
+		persistentFileName = null;
+		
 		/* finally delete table */
 		String createString = "DROP TABLE " + tableName;
-		try{
-		Statement st = db.createStatement();
-		int rs = st.executeUpdate(createString);
-		st.close();		
-		
-		db.close();
+		try {
+			Statement st = db.createStatement();
+			st.executeUpdate(createString);
+			st.close();
+
+			db.close();
+		} catch (SQLException e) {
+			System.err.println("ERROR: SQL Exception!!");
+			System.out.println(e.getMessage());
 		}
-		catch (SQLException e) {
-    		System.err.println("ERROR: SQL Exception!!");
-    		e.printStackTrace();
-    		System.exit(-1);	
-    	}
-		
+
 	}
 
 	public Long get(String key) {
-		
+
 	}
 
 	public void put(String key, Long value) {
@@ -158,12 +158,69 @@ public class QuoteDB {
 	}
 
 	public boolean containsKey(String key) {
-		
+
 		return true;
 	}
 
 	public Long remove(String key) {
+		try {
+			PreparedStatement ps = db.prepareStatement("DELETE FROM "
+					+ tableName + " WHERE ( symbol = " + key + " )");
+			ps.executeUpdate();
+			ps.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		
+		Long returnval = (long) 0;
+		return returnval;
+	}
+
+	public void flush() throws IOException {
+		try {
+			// Write the DB to a temporary file first, and then overwrite,
+			// rather
+			// than accidentally truncating it
+			File cwd = new File(System.getProperty("user.dir"));
+			File tempFile = File.createTempFile("dbtemp", ".tmp", cwd);
+			BufferedWriter quoteOutput = new BufferedWriter(new FileWriter(
+					tempFile));
+
+			// Pull everything out of the database
+			Statement stmt = db
+					.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+							ResultSet.CONCUR_READ_ONLY);
+			ResultSet srs = stmt.executeQuery("SELECT * FROM " + tableName);
+			while (srs.next()) {
+				String name = srs.getString("symbol");
+				int value = srs.getInt("quote");
+				quoteOutput.write(name + " " + value + newline);
+			}
+
+			// Flush the temporary file
+			quoteOutput.flush();
+
+			// Now, move the old DB to a backup file
+			File dbFile = new File(persistentFileName);
+			String backupFileName = persistentFileName.concat(".bak");
+			File backupFile = new File(backupFileName);
+			boolean success = dbFile.renameTo(backupFile);
+			if (!success) {
+				IOException noRename = new IOException("Failed to rename file"
+						+ persistentFileName + " to " + backupFileName);
+				throw noRename;
+			}
+
+			// Rename the temporary file to the database file
+			success = tempFile.renameTo(dbFile);
+			if (!success) {
+				IOException noRename = new IOException(
+						"Failed to rename temp file to " + persistentFileName);
+				throw noRename;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
