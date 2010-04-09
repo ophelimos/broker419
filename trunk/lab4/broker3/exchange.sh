a94ec3e817456d6ed60848a4dab4a2703f443264
robinsonjpr@gmail.com <robinsonjpr@gmail.com@cdf39bd8-06d3-11df-8209-c32e8d2b4809>
2010-04-09 00:46:54 +0000

    No more pg_data

--- trunk/lab4/broker3/exchange.sh
+++ trunk/lab4/broker3/exchange.sh
@@ -5,7 +5,7 @@ JAVA_HOME=${ECE419_HOME}/java/jdk1.6.0
 JOB_LIB_DIR=${ECE419_HOME}/corba/JOB-4.3/lib
 JOB_LIB=${JOB_LIB_DIR}/OB.jar
 
-PROG=broker/Exchange
+PROG=Exchange
 
 echo "starting" ${PROG}
 ${JAVA_HOME}/bin/java -Djava.endorsed.dirs=${JOB_LIB_DIR} -classpath ${JOB_LIB}:classes:. ${PROG} $*
